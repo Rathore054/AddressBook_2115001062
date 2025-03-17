a4671ce1d038cb4b9510d3acf7daa743b2d03bb9
Udit rathore <uditrathore054@gmail.com>
2025-03-17 12:10:50 +0530

    [Udit] Add: Adding files to Section1-UC1

---
+++ AddressBook/Program.cs
@@ -0,0 +1,52 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+
+using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Context;
+using System;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Database Connection
+builder.Services.AddDbContext<ContextRL>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"),
+    new MySqlServerVersion(new Version(8, 0, 41))));
+
+// Add services to the container.
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RepositoryLayer/Context/ContextRL.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer.Context
+{
+    public class ContextRL : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public DbSet<AddressBookEntity> AddressBookEntries { get; set; }
+
+        public ContextRL(DbContextOptions<ContextRL> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AddressBookEntity>()
+                .HasOne(ab => ab.User)
+                .WithMany(u => u.AddressBookEntries)
+                .HasForeignKey(ab => ab.UserId);
+        }
+    }
+}
