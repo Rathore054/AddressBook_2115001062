324886751a08581723f810c906b5b56217921be4
Udit rathore <uditrathore054@gmail.com>
2025-03-17 14:29:44 +0530

    [Udit] Add: adding files to Section1-UC2

---
+++ ModelLayer/Model/RequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelLayer.Model
+{
+    public class RequestModel
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(15)]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress, MaxLength(255)]
+        public string? Email { get; set; }
+
+        public string? Address { get; set; }
+    }
+}
